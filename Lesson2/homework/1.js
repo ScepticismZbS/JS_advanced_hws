26546cf1f449343c0432148b1dff753a27ffcbb7
Anatoliy Anatoliy <ttwinsens@gmail.com>
2021-09-26 18:04:00 +0300

    Lesson 2
    Lesson 2

---
+++ Lesson2/homework/1.js
@@ -0,0 +1,58 @@
+
+  /*
+
+    Задание 1.
+
+    Написать скрипт который будет будет переключать вкладки по нажатию
+    на кнопки в хедере.
+
+    Главное условие - изменять файл HTML нельзя.
+
+    Алгоритм:
+      1. Выбрать каждую кнопку в шапке
+         + бонус выбрать одним селектором
+
+      2. Повесить кнопку онклик
+          button1.onclick = function(event) {
+
+          }
+          + бонус: один обработчик на все три кнопки
+
+      3. Написать функцию которая выбирает соответствующую вкладку
+         и добавляет к ней класс active
+
+      4. Написать функцию hideAllTabs которая прячет все вкладки.
+         Удаляя класс active со всех вкладок
+
+    Методы для работы:
+
+      getElementById
+      querySelector
+      classList
+      classList.add
+      forEach
+      onclick
+
+      element.onclick = function(event) {
+        // do stuff ...
+      }
+
+  */
+
+
+
+      // 1
+
+      let btns = document.querySelectorAll("button");
+      console.log(btns);
+        
+         function show(event) {
+        let numTab = event.target.dataset.tab
+        console.log(numTab)
+      }
+
+      let tabs = document.querySelectorAll(".tab");
+
+      btns.onclick = function() {
+        
+      }
